68f0e72926aa1e1678d38184eb10557451a8a2d7
contexttodata <alexlive123@live.co.uk>
2017-05-05 05:47:11 +0100

    Create dictionaryWrite.java
    a basic scanner to read input from console soon to be a basic editor for the bram backend

---
+++ dictionaryWrite.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+import java.io.File;
+/**
+ * Created by alexl on 5/5/2017.
+ */
+public class dictionaryWrite{
+
+    public static void main() {
+        boolean bp  = true;
+        //try {
+        if(bp == true){
+            Scanner scanner = new Scanner(System.in);
+            String bw = scanner.next();
+            System.out.println(bw); }
+            //BufferedWriter bw = new BufferedWriter(new FileWriter("fileName"));
+           // bw.write();
+         //catch (IOException err) {
+           //err.printStackTrace();
+            else if(bp == false)
+            {System.out.println("welllthis is fucked");}
+        }
+        }
